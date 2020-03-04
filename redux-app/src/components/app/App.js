015d86c5755d675b0467c8f8c32f00e5a4b299ce
Hichem AMAR BENSABER <amar-bensaber.h@sfeir.com>
2020-03-04 11:08:02 +0100

    testing prettier with husky

--- redux-app/src/components/app/App.js
+++ redux-app/src/components/app/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from "react";
-import "./App.css";
-import Home from "../home/Home";
+import React, { Component } from 'react';
+import './App.css';
+import Home from '../home/Home';
 
 class App extends Component {
-  state = {
-    schoolName: "Hetic"
-  };
+    state = {
+        schoolName: 'Hetic',
+    };
 
-  changeSchoolName = () => {
-    this.setState({
-      schoolName: "Epitech"
-    });
-  };
+    changeSchoolName = () => {
+        this.setState({
+            schoolName: 'Polytechnique',
+        });
+    };
 
-  render() {
-    return (
-      <div className="App">
-        <Home
-          schoolName={this.state.schoolName}
-          changeSchoolName={this.changeSchoolName}
-        />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Home
+                    schoolName={this.state.schoolName}
+                    changeSchoolName={this.changeSchoolName}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
