a1495f449ed9b573b2d234274f487b3235b5b8d9
Hichem AMAR BENSABER <amar-bensaber.h@sfeir.com>
2020-03-04 11:47:58 +0100

    testing linter

--- redux-app/src/components/home/Home.js
+++ redux-app/src/components/home/Home.js
@@ -1,35 +1,39 @@
 import React from 'react';
 import './Home.css';
 import Modal from '../modal/Modal';
-import {connect} from 'react-redux';
-import {showModal} from '../../actions/index';
+import { connect } from 'react-redux';
+import { showModal } from '../../actions/index';
 
-const Home = ({schoolName, changeSchoolName, modal, showModal}) => {
-
+const Home = ({ schoolName, changeSchoolName, modal, showModal }) => {
     const onChange = () => {
         changeSchoolName();
     };
 
     return (
         <div className="Home">
-            <h1 className="Home-title">Salut, bienvenue à <span className="Home-title--crimson">{schoolName}</span></h1>
-            <button className="Home-button" onClick={onChange}>Changer le nom</button>
-            <button className="Home-button" onClick={showModal}>Ouvrir la modal</button>
-            { modal &&
-                <Modal/>
-            }
+            <h1 className="Home-title">
+                Salut, bienvenue à{' '}
+                <span className="Home-title--crimson">{schoolName}</span>
+            </h1>
+            <button className="Home-button" onClick={onChange}>
+                Changer le nom
+            </button>
+            <button className="Home-button" onClick={showModal}>
+                Ouvrir la modal
+            </button>
+            {modal && <Modal />}
         </div>
     );
 };
 
-const mapStateToProps = state => {
-    return {modal: state.modal}
+const mapStateToProps = (state) => {
+    return { modal: state.modal };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
     return {
-        showModal: () => dispatch(showModal())
-    }
+        showModal: () => dispatch(showModal()),
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- redux-app/src/components/modal/Modal.js
+++ redux-app/src/components/modal/Modal.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import './Modal.css';
-import {connect} from 'react-redux';
-import {addElement} from '../../actions/index';
-import {handleChange} from '../../actions/index';
+import { connect } from 'react-redux';
+import { addElement } from '../../actions/index';
+import { handleChange } from '../../actions/index';
 import store from '../../store/index';
 
-const Modal = ({array, text, text2, addElement, handleChange}) => {
-
+const Modal = ({ array, text, text2, addElement, handleChange }) => {
     return (
         <div className="Modal">
             <h1 className="Home-title">List</h1>
             <div className="Modal-control">
-                <input className='Modal-input' type="text" name="text" value={text} onChange={handleChange}/>
-                <input className='Modal-input' type="text" name="text2" value={text2} onChange={handleChange}/>
-                <button className='Modal-button' onClick={addElement}>Ajouter l'élement au tableau</button>
+                <input
+                    className="Modal-input"
+                    type="text"
+                    name="text"
+                    value={text}
+                    onChange={handleChange}
+                />
+                <input
+                    className="Modal-input"
+                    type="text"
+                    name="text2"
+                    value={text2}
+                    onChange={handleChange}
+                />
+                <button className="Modal-button" onClick={addElement}>
+                    Ajouter l'élement au tableau
+                </button>
             </div>
             <ul className="Modal-list">
-                {array.map(element => <li className="Modal-list__item">🌲{element}</li>)}
+                {array.map((element) => (
+                    <li className="Modal-list__item">🌲{element}</li>
+                ))}
             </ul>
             <p>{text}</p>
             <p>{text2}</p>
@@ -24,15 +39,16 @@ const Modal = ({array, text, text2, addElement, handleChange}) => {
     );
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
     array: state.array,
     text: state.text,
-    text2: state.text2
+    text2: state.text2,
 });
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
     addElement: () => dispatch(addElement(store.getState().text)),
-    handleChange: (event) => dispatch(handleChange(event.target.name, event.target.value))
+    handleChange: (event) =>
+        dispatch(handleChange(event.target.name, event.target.value)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Modal);
